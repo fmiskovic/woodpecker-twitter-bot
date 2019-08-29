c2ee2bb374e65dd580efc1a096ce54ae841b0706
Filip Miskovic <filipmiskovic@MacBook-Pro-2.local>
2019-08-29 17:54:22 +0200

    newsapi 3

--- news_api_auth.py
+++ news_api_auth.py
@@ -1,7 +1,7 @@
 import logging.config
 import os
 
-import newsapi
+from newsapi import newsapi_client
 
 logging.config.fileConfig('logging.config')
 logger = logging.getLogger(__name__)
@@ -10,4 +10,4 @@ logger = logging.getLogger(__name__)
 def create_api():
     """authenticate to newsapi.org and get NewsApiClient"""
     logger.info('creating News API client...')
-    return newsapi.newsapi_client.NewsApiClient(api_key=os.getenv('NEWS_API_KEY'))
+    return newsapi_client.NewsApiClient(api_key=os.getenv('NEWS_API_KEY'))
