2359d018383cff86bdc2778c35d1f73b2a15876b
fmiskovic <fmiskovic@yandex.com>
2019-09-07 09:38:51 +0200

    fix duplicated tweets

--- twitter/tweet_similarity.py
+++ twitter/tweet_similarity.py
@@ -9,5 +9,6 @@ def are_similar(t1, t2):
 
 
 def are_similar_text(t1, t2):
-    sub1 = t1[0:t1.find('\n\n')]
-    return sub1 == t2
+    # sub1 = t1[0:t1.find('\n\n')]
+    sub1 = t1[0:t1.find('http')]
+    return are_similar(sub1, t2)
