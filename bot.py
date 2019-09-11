1f4ccf3b209848b3b0829bcd03ef19de06d44e0c
fmiskovic <fmiskovic@yandex.com>
2019-09-11 11:55:31 +0200

    improve sleep mechanism

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ import time
 from news import news_grabber, news_api_auth
 from twitter import followers, tweets, twitter_api_auth
 
-INTERVAL = 60 * 60  # sleep time interval is 60 minutes
+LONG_INTERVAL = 60 * 60  # sleep time interval is 60 minutes
+SHORT_INTERVAL = 60 * 2  # sleep time interval is 2 minutes
 
 logging.config.fileConfig('logging.config')
 logger = logging.getLogger('bot')
@@ -22,23 +23,32 @@ def get_news(query=None, category=None, country=None, language='en'):
     return news_grabber.get_news(query=query, category=category, country=country, language=language)
 
 
-def tweet_news(news_items, hash_tag=None):
-    for n in news_items:
-        result = tw_handler.post_new_tweet(text=n.description, source=n.url, hashtag=hash_tag)
-        if result is not None:
-            logging.info(f'Taking a %s minutes break...', INTERVAL / 60)
-            time.sleep(INTERVAL)
-            logger.info('OK I am ready to continue. Lets tweet something again...')
+def tweet_news(news_item, hash_tag=None):
+    return tw_handler.post_new_tweet(text=news_item.description, source=news_item.url, hashtag=hash_tag)
 
 
 keywords = ['cryptocurrency', 'ethereum', 'bitcoin', 'XRP', 'litecoin']
 
 while True:
     # follow new followers
-    # followers_handler.follow_followers()
+    followers_handler.follow_followers()
 
+    # choose random keyword
     keyword = random.choice(keywords)
+
     news = get_news(query=keyword, language='en')
+    if len(news) == 0:
+        # if there are no news, take a short break
+        logger.info(f'I did not find any news for keyword %s', keyword)
+        logger.info(f'Taking a %s minutes break...', SHORT_INTERVAL / 60)
+        time.sleep(SHORT_INTERVAL)
+
     if len(news) > 0:
         logger.info('Tweet attempt about ' + keyword)
-        tweet_news(news_items=news, hash_tag='#cryptocurrencynews #' + keyword)
+        hashtag = '#cryptocurrencynews #' + keyword
+        for n in news:
+            result = tweet_news(news_item=n, hash_tag=hashtag)
+            if result is not None:
+                logging.info(f'Taking a %s minutes break...', LONG_INTERVAL / 60)
+                time.sleep(LONG_INTERVAL)
+                logger.info('OK I am ready to continue. Lets tweet something again...')
